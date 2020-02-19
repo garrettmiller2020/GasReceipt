4e512ea30284ba9f9cb80914299d7af74f5e93ef
Garrett Miller <garrett.miller.2020@student.lhschools.org>
2020-02-19 12:56:36 -0500

    Intelij Commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,31 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+       System.out.println("+---------------------------------+");
+       System.out.println("|      LotsOGas Gas Station       |");
+       System.out.println("|       15634 Summit Road         |");
+       System.out.println("|   Nelson, Ohio United States    |");
+       System.out.println("|                                 |");
+       System.out.println("|Date            Time             |");
+       System.out.println("|05/25/18        1:03P.M.         |");
+       System.out.println("|Invoice Number#          324516  |");
+       System.out.println("|MasterCard                       |");
+       System.out.println("|***********35                    |");
+       System.out.println("|                                 |");
+       System.out.println("|Pump         Product       $/G   |");
+       System.out.println("|04           Unleaded      $2.34 |");
+       System.out.println("|(G)          Fuel Total          |");
+       System.out.println("|4.0 Gal      $9.36               |");
+       System.out.println("|                                 |");
+       System.out.println("|35 Approved - Thank You          |");
+       System.out.println("|                                 |");
+       System.out.println("|        -- Important --          |");
+       System.out.println("|Retain This Copy For Your Record |");
+       System.out.println("|                                 |");
+       System.out.println("|     Thanks For Shopping At      |");
+       System.out.println("|           LotsOGas              |");
+       System.out.println("+---------------------------------+");
+    }
+}
